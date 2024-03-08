64b47b4b6291b312c69b5002d275fa01142f767a
Lucas Vitor <52681526+chemical000@users.noreply.github.com>
2024-03-08 10:32:22 -0300

    Icone no navegador

--- register/register.php
+++ register/register.php
@@ -7,6 +7,7 @@
         <link rel="stylesheet" href="../login/style.css">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" type="image/x-icon" href="../logo/3.png">
         <title>Crie sua conta!</title>
     </head>
     <body>
